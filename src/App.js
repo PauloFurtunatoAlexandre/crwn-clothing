f975180bcedd6a20ffdcad196bc18d7784f4edb8
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-06-15 22:24:04 -0500

    added data, shoppage, collections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./components/homepage/Homepage.component";
+import ShopPage from "./components/ShopPage/ShopPage.component";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/homepage/Homepage.component.jsx
+++ src/components/homepage/Homepage.component.jsx
@@ -3,11 +3,11 @@ import "./homepage.styles.scss";
 import Directory from "../directory/Directory.component";
 
 const HomePage = () => {
-  return(
+  return (
     <div className="homepage">
       <Directory />
     </div>
   );
-}
+};
 
 export default HomePage;
