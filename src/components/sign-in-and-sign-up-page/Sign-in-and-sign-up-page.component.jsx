d1436bf3275fb27fff15acea0f05cc1d47987c01
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-06-17 15:29:14 -0500

    added label value and fixed the form

---
+++ src/components/sign-in-and-sign-up-page/Sign-in-and-sign-up-page.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.styles.scss";
+
+import SignIn from "../sign-in/Sign-in.component";
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sig-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
