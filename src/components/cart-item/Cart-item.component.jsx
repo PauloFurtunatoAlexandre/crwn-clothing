578f34d80b2ccedbfb22e606db134d7e45c5d6ca
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-13 14:33:34 -0500

    created the cart item component and status via cart reducer

---
+++ src/components/cart-item/Cart-item.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./Cart-item.styles.scss";
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+  return (
+    <div className="cart-item">
+      <img className="cart-item-image" src={imageUrl} alt="item" />
+      <div className="item-details">
+        <span className="name">{name} </span>
+        <span className="price">
+          {quantity} x $ {price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
