17d7f85bb68f8f11b401cdbf5405c6f97f463d53
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-20 18:55:33 -0500

    added directory reducer and type, added the remove button in the cart dropdown

--- src/components/cart-item/Cart-item.component.jsx
+++ src/components/cart-item/Cart-item.component.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 
+import { connect } from "react-redux";
+
+import {
+  clearItemFromCart,
+  // addItem,
+  // removeItem,
+} from "../../redux/cart/cart.actions";
+
 import "./Cart-item.styles.scss";
 
-const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+const CartItem = ({ item, cleartItem }) => {
+  const { imageUrl, price, name, quantity } = item;
   return (
     <div className="cart-item">
       <img className="cart-item-image" src={imageUrl} alt="item" />
@@ -11,9 +20,18 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
         <span className="price">
           {quantity} x $ {price}
         </span>
+        <span className="remove-button" onClick={() => cleartItem(item)}>
+          <span className="remove-text">Remove Item(s)</span> &#10006;
+        </span>
       </div>
     </div>
   );
 };
 
-export default CartItem;
+const mapDispatchToProps = (dispatch) => ({
+  cleartItem: (item) => dispatch(clearItemFromCart(item)),
+  // addItem: (item) => dispatch(addItem(item)),
+  // removeItem: (item) => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CartItem);
