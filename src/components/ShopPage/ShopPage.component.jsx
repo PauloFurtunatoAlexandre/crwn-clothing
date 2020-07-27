e629d9aa8015b92b8f82f3fddaaabdb8758795da
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-27 12:27:57 -0500

    fixed the wrong commit

--- src/components/ShopPage/ShopPage.component.jsx
+++ src/components/ShopPage/ShopPage.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../collections-overview/Collections-overview.component";
 
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    <CollectionsOverview collections={collections} />
+    <Route exact path={} component={CollectionsOverview} />
   </div>
 );
 
