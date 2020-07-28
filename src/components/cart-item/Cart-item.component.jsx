f9e19ecf3bdebb99952b36e518bfd19d1d628ce2
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-28 17:40:48 -0500

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with ux flow for directory/shop

--- src/components/cart-item/Cart-item.component.jsx
+++ src/components/cart-item/Cart-item.component.jsx
@@ -1,31 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import { connect } from "react-redux";
+import './cart-item.styles.scss';
 
-import { clearItemFromCart } from "../../redux/cart/cart.actions";
-
-import "./cart-item.styles.scss";
-
-const CartItem = ({ item, cleartItem }) => {
-  const { imageUrl, price, name, quantity } = item;
-  return (
-    <div className="cart-item">
-      <img className="cart-item-image" src={imageUrl} alt="item" />
-      <div className="item-details">
-        <span className="name">{name} </span>
-        <span className="price">
-          {quantity} x $ {price}
-        </span>
-        <span className="remove-button" onClick={() => cleartItem(item)}>
-          <span className="remove-text">Remove Item(s)</span> &#10006;
-        </span>
-      </div>
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <div className='cart-item'>
+    <img src={imageUrl} alt='item' />
+    <div className='item-details'>
+      <span className='name'>{name}</span>
+      <span className='price'>
+        {quantity} x ${price}
+      </span>
     </div>
-  );
-};
+  </div>
+);
 
-const mapDispatchToProps = (dispatch) => ({
-  cleartItem: (item) => dispatch(clearItemFromCart(item)),
-});
-
-export default connect(null, mapDispatchToProps)(CartItem);
+export default CartItem;
