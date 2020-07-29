7129b0595e8f1e7c9f9a879e90fc2517f67c2843
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-28 23:11:37 -0500

    removed firebase utils from the name of firebase utils

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/utils.js';
 
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
